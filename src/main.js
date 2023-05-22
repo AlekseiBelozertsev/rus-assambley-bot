e4c2be8d944153e712d4760ae5125a749135e01e
alexeibelozertsev <belozertsev.alexei@gmail.com>
2023-05-22 22:57:57 +0200

    chore: env

---
+++ src/main.js
@@ -0,0 +1,35 @@
+import { Telegraf } from 'telegraf';
+import { config } from 'dotenv';
+
+config();
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+export const start = () => {
+  // Start command handler
+  bot.start((ctx) => {
+    const { first_name } = ctx.from;
+    ctx.reply(`Привет, ${first_name}! Тестовая версия бота МАРАА приветствует тебя.`);
+  });
+
+  // Help command handler
+  bot.help((ctx) => {
+    ctx.reply('Это тестовая версия. Можешь выбрать команды /start или /help.');
+  });
+
+  // Custom command handler
+  bot.command('custom', (ctx) => {
+    ctx.reply('Упс, ты нажал /custom. Эта команда в разработке.');
+  });
+
+// Start the bot
+  bot.launch()
+    .then(() => {
+      console.log('Bot is running');
+    })
+    .catch((error) => {
+      console.error('Error starting bot', error);
+    });
+}
+
+
