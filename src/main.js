7d1152c66b0add0bf14b3c75170d77b521bff224
alexeibelozertsev <belozertsev.alexei@gmail.com>
2023-06-12 13:16:28 +0200

    feat: message handler updated with reply

--- src/main.js
+++ src/main.js
@@ -5,32 +5,56 @@ config();
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
+
 export const start = () => {
   // Start command handler
   bot.start((ctx) => {
     const { first_name } = ctx.from;
-    ctx.reply(`Привет, ${first_name}! Тестовая версия бота МАРАА приветствует тебя.`);
-    
+    const commands = [
+      '/start - Запуск бота',
+      '/help - Помощь',
+      '/sendmessage - Послать сообщение',
+
+    ]
+    const commandList = commands.join('\n');
+    ctx.reply(`Привет, ${first_name}! Тестовая версия бота МАРАА приветствует тебя.\n Доступные команды:\n ${commandList}`);
   });
 
   // Send message comand send message to the chat
-  bot.command('sendmessage', (ctx) => {
-    const chatId = process.env.CHAT_ID
-    const messageText = 'Послано в чат из бота.';
-    ctx.telegram.sendMessage(chatId, messageText);
-  })
+  bot.command('sendmessage', async (ctx) => {
+    const chatId = process.env.CHAT_ID;
+  
+    // Prompt the user for their message
+    await ctx.reply('Введите сообщение, которое вы хотите отправить:');
+  
+    // Listen for the user's reply
+    bot.on('message', async (ctx) => {
+      if (ctx.chat.type === 'private') {
+        const message = ctx.message;
+        bot.telegram.sendMessage(chatId, message.text);
+        await ctx.reply('Сообщение послано в чат.');
+      };
+      // If replied to messsage - copy message and send back to bot
+      if (ctx.message.reply_to_message) {
+        const user_id = ctx.message.from.id;
+
+        await ctx.telegram
+        .copyMessage(
+          user_id,
+          ctx.message.chat.id,
+          ctx.message.message_id
+        );
+        await ctx.reply('Сообщение послано юзеру.');
+      }
+    });
+  });
 
   // Help command handler
   bot.help((ctx) => {
-    ctx.reply('Это тестовая версия. Можешь выбрать команды /start или /help.');
+    ctx.reply('Тут будет инструкция по использованию бота и его описание');
   });
 
-  // Custom command handler
-  bot.command('custom', (ctx) => {
-    ctx.reply('Упс, ты нажал /custom. Эта команда в разработке.');
-  });
-
-// Start the bot
+  // Start the bot
   bot.launch()
     .then(() => {
       console.log('Bot is running');
@@ -38,6 +62,7 @@ export const start = () => {
     .catch((error) => {
       console.error('Error starting bot', error);
     });
+
 }
 
 
