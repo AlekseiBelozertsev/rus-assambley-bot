3e8b841ed00402eaf8e9480826f4f85715c72c58
alexeibelozertsev <belozertsev.alexei@gmail.com>
2023-06-21 11:52:15 +0200

    feat: added error handling

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { Telegraf } from 'telegraf';
+import { Telegraf, Markup } from 'telegraf';
 import { config } from 'dotenv';
 
 config();
@@ -10,44 +10,58 @@ export const start = () => {
   // Start command handler
   bot.start((ctx) => {
     const { first_name } = ctx.from;
-    const commands = [
-      '/start - Запуск бота',
-      '/help - Помощь',
-      '/sendmessage - Послать сообщение',
-
-    ]
-    const commandList = commands.join('\n');
-    ctx.reply(`Привет, ${first_name}! Тестовая версия бота МАРАА приветствует тебя.\n Доступные команды:\n ${commandList}`);
+    const greetMessage = `Привет, ${first_name}! Тестовая версия бота МАРАА приветствует тебя. Список доступных комманд вы найдёте в меню бота рядом полем для ввода сообщений.`;
+    ctx.reply(greetMessage);
   });
 
-  // Send message comand send message to the chat
-  bot.command('sendmessage', async (ctx) => {
-    const chatId = process.env.CHAT_ID;
-  
-    // Prompt the user for their message
-    await ctx.reply('Введите сообщение, которое вы хотите отправить:');
-  
-    // Listen for the user's reply
-    bot.on('message', async (ctx) => {
+// Send message command to send a message to the chat
+bot.command('sendmessage', async (ctx) => {
+  const chatId = process.env.CHAT_ID;
+  const replyButton = Markup.inlineKeyboard([
+    Markup.button.url('Ответить', `callback_data`)
+  ]);
+
+  // Prompt the user for their message
+  await ctx.reply('Введите сообщение, которое вы хотите отправить:');
+
+  // Listen for the user's reply
+  bot.on('message', async (ctx) => {
+    try {
       if (ctx.chat.type === 'private') {
         const message = ctx.message;
-        bot.telegram.sendMessage(chatId, message.text);
-        await ctx.reply('Сообщение послано в чат.');
-      };
-      // If replied to messsage - copy message and send back to bot
+        try {
+          await bot.telegram.sendMessage(chatId, message.text);
+          await ctx.reply('Сообщение отправлено.');
+        } catch (error) {
+          console.error('An error occurred while sending the message:', error);
+          await ctx.reply('Произошла ошибка при отправке. Пожалуйста, посылайте только сообщения.');
+        }
+      }
+      // If replied to a message - copy the message and send it back to the bot
       if (ctx.message.reply_to_message) {
         const user_id = ctx.message.from.id;
 
-        await ctx.telegram
-        .copyMessage(
+        await ctx.telegram.copyMessage(
           user_id,
           ctx.message.chat.id,
           ctx.message.message_id
         );
-        await ctx.reply('Сообщение послано юзеру.');
+
+        // Add the reply button below the copied message
+        const copiedMessageId = ctx.message.reply_to_message.message_id;
+        await ctx.telegram.sendMessage(
+          chatId,
+          'Сообщение отправлено.',
+          { reply_to_message_id: copiedMessageId, reply_markup: replyButton }
+        );
       }
-    });
+    } catch (error) {
+      console.error('An error occurred:', error);
+      await ctx.reply('Произошла ошибка. Пожалуйста, попробуйте еще раз позже.');
+    }
   });
+});
+
 
   // Help command handler
   bot.help((ctx) => {
