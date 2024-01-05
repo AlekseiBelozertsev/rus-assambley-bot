a5ef08cfbae40cd0c4a45f58c08965ba9ea60b26
alexeibelozertsev <belozertsev.alexei@gmail.com>
2024-01-05 09:56:27 +0100

    fix: formatting

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import { config } from 'dotenv';
-import { Telegraf } from 'telegraf';
-import { errorMessage, initialMessage, sendMessage } from './text.js';
+import { config } from "dotenv";
+import { Telegraf } from "telegraf";
+import { errorMessage, initialMessage, sendMessage } from "./text.js";
 
 export class Bot {
   constructor() {
@@ -17,27 +17,25 @@ export class Bot {
       ctx.reply(initialMessage);
     });
 
-    //notifies when new user started chating with the bot
-    this.bot.on('new_chat_members', async (ctx) => {
-      const user = ctx.message.new_chat_members[0];
-      const chatId = this.chatId;
-      const notificationMessage = `${user.first_name} подключился к боту.`;
-      ctx.telegram.sendMessage(chatId, notificationMessage);
-    })
-
     // Listen for the user's reply
-    this.bot.on('message', async (ctx) => {
+    this.bot.on("message", async (ctx) => {
       try {
         const message = ctx.message;
         if (message.text) {
-          if (message.chat.type === 'private') {
+          if (message.chat.type === "private") {
             try {
-              await ctx.telegram.sendMessage(this.chatId, `Cообщение от ${ctx.from.first_name}: ${message.text}`);
+              await ctx.telegram.sendMessage(
+                this.chatId,
+                `Cообщение от ${ctx.from.first_name}: ${message.text}`
+              );
               this.user_id = ctx.message.from.id; // Store the user_id
               await ctx.reply(sendMessage);
             } catch (error) {
-              console.error('An error occurred while sending the message:', error);
-              await ctx.reply(errorMessage);
+              console.error(
+                "An error occurred while sending the message:",
+                error
+              );
+              await ctx.reply("Суперошибка");
             }
           }
           // If replied to a message - copy the message and send it back to the bot and then notifies the chat member, that the message has been sent
@@ -45,29 +43,28 @@ export class Bot {
             await ctx.telegram.copyMessage(
               this.user_id, // Use the stored user_id
               this.chatId,
-              message.message_id,
+              message.message_id
             );
-            await ctx.telegram.sendMessage(
-              this.chatId,
-              sendMessage,
-            );
+            await ctx.telegram.sendMessage(this.chatId, sendMessage);
           }
         } else {
           ctx.reply(errorMessage);
         }
       } catch (error) {
-        console.error('An error occurred:', error);
+        console.error("An error occurred:", error);
       }
     });
 
     // Start the bot
-    this.bot.launch({
-      webhook: {
-        domain: process.env.APP_URL,
-        port: this.port,
-      },
-    }).then(() => {
-      console.log(`Bot is running on port ${this.port}`);
-    });
+    this.bot
+      .launch({
+        webhook: {
+          domain: process.env.APP_URL,
+          port: this.port,
+        },
+      })
+      .then(() => {
+        console.log(`Bot is running on port ${this.port}`);
+      });
   }
 }
