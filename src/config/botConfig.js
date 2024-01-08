003a1a675742c76a91ddf2bb3440cb59666762b5
alexeibelozertsev <belozertsev.alexei@gmail.com>
2024-01-08 16:39:07 +0100

    feat: bot config for prod and dev

---
+++ src/config/botConfig.js
@@ -0,0 +1,25 @@
+import { config } from "dotenv";
+import { data } from "../data/data.js";
+
+export class BotConfig {
+  static configure(configType) {
+    config();
+    switch (configType) {
+      case "dev":
+        return {
+          port: process.env.PORT || 5001,
+          botToken: process.env.DEV_BOT_TOKEN,
+          chatId: process.env.DEV_CHAT_ID,
+        };
+      case "prod":
+        return {
+          port: process.env.PORT || 5001,
+          botToken: process.env.BOT_TOKEN,
+          chatId: process.env.CHAT_ID,
+          appUrl: process.env.APP_URL
+        };
+      default:
+        throw new Error(data.invalidConfig);
+    }
+  }
+}
